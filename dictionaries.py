4e83482b91c86c8c80141bf2495309ae4a887bba
rrodzstgo <ronalderodriguez@gmail.com>
2020-05-18 20:34:01 -0400

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,15 +1,14 @@
 string_to_dictionary_file = open("dictionary_file","r")
 
 content = string_to_dictionary_file.read()
-print(content)
-
 
 dict_words = content.split(' ')
-counts = dict()
-for word in dict_words:
-    if word in dict_words:
-        counts[word] += 1
-    else:
-        counts[word] = 1
 
-    print(counts)
+from collections import Counter
+
+counted_dictionary = Counter(dict_words)
+
+for key, entry in counted_dictionary.items():
+    print(key)
+    print(entry)
+
